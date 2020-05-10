c84d05aad9f168cb93f014ca9446e5812789ab18
richu <ricabrah@cisco.com>
2020-05-10 23:51:06 +0530

    made changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bin/webpage
+web: bin/main
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+	"os"
+	"github.com/gorilla/mux"
+)
+
+var randomValue string
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("Sample webpage time : " + time.Now().String() + "\n" + "containerRandomNumber = " + randomValue))
+}
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	max := 50
+	randomValue = strconv.Itoa(rand.Intn(max-min+1) + min)
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	http.ListenAndServe(":"+os.Getenv("PORT"), r)
+}
