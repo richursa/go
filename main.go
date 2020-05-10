222c3a9157186c2d7bb4fe18f2fea0c5ebd16b7e
richu shaji abraham <richursa@gmail.com>
2020-05-11 00:55:47 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 )
 
 var randomValue string
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Sample webpage time : " + time.Now().String() + "\n" + "containerRandomNumber = " + randomValue + "\nHAAAI GAUTHAM \nENTHARUND CHELLAKILI\nSUGHANGALOKKE THANNE ?"))
